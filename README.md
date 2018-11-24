9359acdf4b664e8b770c0d641058553e565fd4f4
shulha <shulhad@gmail.com>
2018-11-24 22:05:27 +0200

    created http-server

---
+++ README.md
@@ -0,0 +1,8 @@
+routes:
+
+    /variables - response Object ENV
+    
+    /variables/:name - response String ENV['name']
+    
+    /files/:name - response File name.pdf 
+
--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 const env = require('./getEnv');
 const http = require('http');
+const url = require('url');
 
 const PORT = 3000;
 
-const server = http.createServer((request, response) => {
-    const { remoteAddress, remotePort } = response.socket;
-    console.log(`Requested ${request.method} ${request.url} from ${remoteAddress}:${remotePort}`);
+function getFateTime() {
+    return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 
-    response.setHeader('Content-Type', 'application/json');
-    response.statusCode = 201;
-    response.end(env);
+}
+
+function log(req, res) {
+    const { remoteAddress, remotePort } = res.socket;
+    const parsedURL = url.parse(req.url);
+    //дату и время запроса, метод, URL, параметры запроса, ip-адрес
+    console.log(`${getFateTime()} - ${req.method} to ${parsedURL.pathname} with ${parsedURL.query} from ${remoteAddress}:${remotePort}`);
+}
+
+const server = http.createServer((request, response) => {
+    const splitURL = url.parse(request.url).pathname.split('/');
+    const baseURL = splitURL[1];
+    const nameURL = splitURL[2];
+
+    log(request, response);
+
+    if (baseURL === 'variables') {
+        if (nameURL) {
+            const envJSON = JSON.parse(env);
+            const envValue = envJSON[nameURL];
+
+            if (envValue) {
+                response.writeHead(200,{'Content-Type' : 'text/plain'});
+                response.end(envValue);
+            } else {
+                response.writeHead(404,{'Content-Type' : 'text/plain'});
+                response.end('Not found');
+            }
+        } else {
+            response.writeHead(200,{'Content-Type' : 'application/json'});
+            response.end(env);
+        }
+    } else if (baseURL === 'files') {
+        if (nameURL) {
+            // response.setHeader('Content-Type', 'application/pdf');
+            response.statusCode = 200;
+            response.end(nameURL + ' file');
+        } else {
+            response.writeHead(404,{'Content-Type' : 'text/plain'});
+            response.end('Not found');
+        }
+    } else {
+        response.writeHead(400,{'Content-Type' : 'text/plain'});
+        response.end('Bad request');
+    }
 });
 
 server.listen(PORT, err => {
